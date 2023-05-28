5d8ea4160fe16203e408ce2ac8e63f8f284733df
yarik-firefly <133967706+yarik-firefly@users.noreply.github.com>
2023-05-28 15:51:17 +0300

    Update game.js

--- js/game.js
+++ js/game.js
@@ -214,7 +214,7 @@ const listBtn = btn.addEventListener("click", () => {
   // Звук клика
   audioOne.play();
 
-  if (valueInput.value == randomNumber) {
+  if (valueInput.value === randomNumber) {
     // Показываем Верно над квадратом
     showInscription.innerHTML = "Right!";
 
